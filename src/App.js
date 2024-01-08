cdcdd36b3dcfba3d1d97684b8ad7935541f72951
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2024-01-08 07:44:26 +0300

    Set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
 };
 
 export default App;

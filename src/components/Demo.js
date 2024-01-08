411f847ddae89d2fee7191b9a82f743af4e80881
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2024-01-08 08:45:42 +0300

    Redux toolkit

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {};
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src=""
+            alt="link icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,28 @@
-import React from "react";
-
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src="" alt="sumz logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/Kalutu/ai-summarizer")}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4 </span>
+      </h1>
+
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concose summaries
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
